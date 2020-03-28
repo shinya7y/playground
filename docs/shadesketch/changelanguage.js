dc95a253b776377cb0b0707dd583c39762cbdb8f
Yosuke Shinya <42844407+shinya7y@users.noreply.github.com>
2020-03-29 02:38:03 +0900

    [ShadeSketch] Add CPU backend. Update using tfjs API. Fix language.

--- docs/shadesketch/changelanguage.js
+++ docs/shadesketch/changelanguage.js
@@ -3,8 +3,13 @@ window.onload = function() {
   langSet(wDef);
 }
 function langSet(argLang){
+  if (argLang != "ja") {
+    argLang = "en";
+  }
+  document.getElementById(argLang + "LangRadio").checked = true;
+
   var elm = document.getElementsByClassName("langCng");
-  for (var i = 0; i < elm.length; i++) {
+  for (let i = 0; i < elm.length; i++) {
     if(elm[i].getAttribute("lang") == argLang){
       elm[i].style.display = '';
     }
--- docs/shadesketch/shadesketch.js
+++ docs/shadesketch/shadesketch.js
@@ -14,33 +14,75 @@ const shaderModelPath = {
   '16': modelDir + 'lineshader16/model.json',
   '32': modelDir + 'lineshader32/model.json',
 };
-var shouldLoadedModels = true;
-var normModel, shaderModel;
+var currentShaderModelPath = null;
+var normModel = null;
+var shaderModel = null;
+
+resetBackend();
+
+async function resetBackend() {
+  tf.disposeVariables();
+  var backendName = document.getElementById('backend_name').value;
+  if (backendName !== 'auto') {
+    await tf.setBackend(backendName).catch((err) => {
+      console.error(err);
+      backendName = 'auto';
+    });
+  }
+
+  if (backendName === 'auto') {
+    tf.backend();
+    await tf.ready();
+  }
+
+  console.log('backend:', tf.getBackend());
+  tf.enableProdMode();
+}
 
 async function loadModels(shaderBit) {
-  console.log("Start loading model");
-  normModel = await tf.loadGraphModel(normModelPath);
-  shaderModel = await tf.loadGraphModel(shaderModelPath[shaderBit]);
-  console.log("Finish loading model");
-  shouldLoadedModels = false;
+  if (normModel === null) {
+    console.log('Start loading normModel');
+    normModel = await tf.loadGraphModel(normModelPath);
+    console.log('Finish loading normModel');
+  }
+  if (currentShaderModelPath !== shaderModelPath[shaderBit]) {
+    console.log('Start loading shaderModel');
+    currentShaderModelPath = shaderModelPath[shaderBit];
+    shaderModel = await tf.loadGraphModel(currentShaderModelPath);
+    console.log('Finish loading shaderModel');
+  }
 }
 
 function cond_to_pos(cond) {
   const cond_pos_rel = {
     '002': [0, 0, -1],
-    '110': [0, 1, -1], '210': [1, 1, -1], '310': [1, 0, -1], '410': [1, -1, -1], '510': [0, -1, -1],
-    '610': [-1, -1, -1], '710': [-1, 0, -1], '810': [-1, 1, -1],
-    '120': [0, 1, 0], '220': [1, 1, 0], '320': [1, 0, 0], '420': [1, -1, 0], '520': [0, -1, 0], '620': [-1, -1, 0],
-    '720': [-1, 0, 0], '820': [-1, 1, 0],
-    '130': [0, 1, 1], '230': [1, 1, 1], '330': [1, 0, 1], '430': [1, -1, 1], '530': [0, -1, 1], '630': [-1, -1, 1],
-    '730': [-1, 0, 1], '830': [-1, 1, 1],
+    '110': [0, 1, -1], '210': [1, 1, -1], '310': [1, 0, -1], '410': [1, -1, -1],
+    '510': [0, -1, -1], '610': [-1, -1, -1], '710': [-1, 0, -1], '810': [-1, 1, -1],
+    '120': [0, 1, 0], '220': [1, 1, 0], '320': [1, 0, 0], '420': [1, -1, 0],
+    '520': [0, -1, 0], '620': [-1, -1, 0], '720': [-1, 0, 0], '820': [-1, 1, 0],
+    '130': [0, 1, 1], '230': [1, 1, 1], '330': [1, 0, 1], '430': [1, -1, 1],
+    '530': [0, -1, 1], '630': [-1, -1, 1], '730': [-1, 0, 1], '830': [-1, 1, 1],
     '001': [0, 0, 1]
   };
   return cond_pos_rel[cond];
 }
 
+async function getLightPosition() {
+  var lightPositionStr = '810'
+  const lpos = document.getElementsByName('lpos');
+  for (let i=0; i<lpos.length; i++) {
+    if (lpos[i].checked){
+      lightPositionStr = lpos[i].value;
+      break;
+    }
+  }
+  const lightPosition = cond_to_pos(lightPositionStr);
+  console.log('Light Position:', lightPositionStr, lightPosition);
+  return lightPosition;
+}
+
 function limitImageSize(size) {
-  const maxSizeStr = document.getElementById("max_size").value;
+  const maxSizeStr = document.getElementById('max_size').value;
   const maxSize = parseInt(maxSizeStr, 10);
   var limitedSize = size;
   if (size > maxSize) {
@@ -54,11 +96,11 @@ function loadSampleImage(canvasName) {
   var context = canvas.getContext('2d');
   var chosenImage = new Image();
   var sampleImages = [
-    "./sample_images/1.png",
-    "./sample_images/6.png",
-    "./sample_images/56736941_p1_line.png",
-    "./sample_images/5895302_p0_line.png",
-    "./sample_images/56537426_p0_line.png",
+    './sample_images/1.png',
+    './sample_images/6.png',
+    './sample_images/56736941_p1_line.png',
+    './sample_images/5895302_p0_line.png',
+    './sample_images/56537426_p0_line.png',
   ];
   chosenImage.src = sampleImages[Math.floor(Math.random() * sampleImages.length)];
   chosenImage.onload = function() {
@@ -85,88 +127,57 @@ function loadLocalImage(files, canvasName) {
 }
 
 async function executeShading() {
-  var lightPositionStr = "810"
-  const lpos = document.getElementsByName("lpos");
-  for(let i=0; i<lpos.length; i++) {
-    if(lpos[i].checked){
-      lightPositionStr = lpos[i].value;
-      break;
-    }
-  }
-  const lightPosition = cond_to_pos(lightPositionStr);
-  console.log("Light Position:", lightPositionStr, lightPosition);
-
+  const lightPosition = await getLightPosition();
   const lineCanvas = document.getElementById('line_canvas');
-  const lineContext = lineCanvas.getContext('2d');
   const imageW = lineCanvas.width;
   const imageH = lineCanvas.height;
-  const lineImageData = lineContext.getImageData(0, 0, imageW, imageH);
   var shadeCanvas = document.getElementById('shade_canvas');
-  var shadeContext = shadeCanvas.getContext('2d');
   shadeCanvas.width  = imageW;
   shadeCanvas.height = imageH;
-  var shadeImageData = shadeContext.createImageData(imageW, imageH)
   var overlayCanvas = document.getElementById('overlay_canvas');
-  var overlayContext = overlayCanvas.getContext('2d');
   overlayCanvas.width  = imageW;
   overlayCanvas.height = imageH;
-  var overlayImageData = overlayContext.createImageData(imageW, imageH)
 
-  var grayLineData = new Float32Array(lineImageData.data.length / 4);
-  for(let i=0; i<lineImageData.data.length; i+=4) {
-    grayLineData[i / 4] = (
-      lineImageData.data[i + 0] * 0.299 +
-      lineImageData.data[i + 1] * 0.587 +
-      lineImageData.data[i + 2] * 0.114) / 255.0;
-  }
+  const rgbLineTensor = tf.browser.fromPixels(lineCanvas).div(tf.scalar(255));
+  // RGB2GRAY
+  const lineTensor = tf.sum(rgbLineTensor.mul(tf.tensor1d([0.299, 0.587, 0.114])), 2);
+  tf.dispose(rgbLineTensor);
 
-  const shaderBit = document.getElementById("shader_bit").value;
-  if (shouldLoadedModels) {
-    await loadModels(shaderBit);
-  }
+  const shaderBit = document.getElementById('shader_bit').value;
+  await loadModels(shaderBit);
 
   // Line norm
-  console.log("Start line normalization");
+  console.log('Start line normalization');
   const normTensor = tf.tidy(() => {
-    const lineTensor = tf.tensor4d(grayLineData, [1, imageH, imageW, 1]);
-    const normResult = normModel.predict(lineTensor);
+    const normResult = normModel.predict(lineTensor.reshape([1, imageH, imageW, 1]));
     // inverse black-in-white lines to white-in-black
     return tf.sub(tf.scalar(1), normResult);
   });
-  console.log("Finish line normalization");
+  console.log('Finish line normalization');
 
   // Line shade
-  console.log("Start shading");
+  console.log('Start shading');
+  const startTime = performance.now();
   const shaderTensor = tf.tidy(() => {
     const lightPositionTensor = tf.tensor2d(lightPosition, [1, 3]);
-    return shaderModel.predict({
+    const shaderPredTensor = shaderModel.predict({
       'input_1' : lightPositionTensor,
       'input_2' : normTensor
-    });
+    }).squeeze([0, 3]);
+    // inverse white-in-black shadow to black-in-white
+    // -1 -> 1 (white background), 1 -> 0 (black shadow)
+    return shaderPredTensor.mul(tf.scalar(-0.5)).add(tf.scalar(0.5));
   });
-  const shaderResult = shaderTensor.dataSync();
-  console.log("Finish shading");
+  const finishTime = performance.now();
+  const elapsedTime = finishTime - startTime;
+  console.log('Finish shading\nTime:%f msec', elapsedTime);
+
+  const overlayTensor = tf.add(lineTensor.mul(tf.scalar(0.8)), shaderTensor.mul(tf.scalar(0.2)));
+  await tf.browser.toPixels(shaderTensor, shadeCanvas);
+  await tf.browser.toPixels(overlayTensor, overlayCanvas);
+
+  tf.dispose(lineTensor);
   tf.dispose(normTensor);
   tf.dispose(shaderTensor);
-
-  // post-process
-  for(let y=0; y<imageH; y++) {
-    for(let x=0; x<imageW; x++) {
-      let i = y * imageW + x;
-      // inverse white-in-black shadow to black-in-white
-      shadeGrayColor = (1 - (shaderResult[i] + 1) / 2) * 255;
-      shadeImageData.data[i*4 + 0] = shadeGrayColor;
-      shadeImageData.data[i*4 + 1] = shadeGrayColor;
-      shadeImageData.data[i*4 + 2] = shadeGrayColor;
-      shadeImageData.data[i*4 + 3] = 255;
-      overlayImageData.data[i*4 + 0] = lineImageData.data[i*4 + 0] * 0.8 + shadeGrayColor * 0.2;
-      overlayImageData.data[i*4 + 1] = lineImageData.data[i*4 + 1] * 0.8 + shadeGrayColor * 0.2;
-      overlayImageData.data[i*4 + 2] = lineImageData.data[i*4 + 2] * 0.8 + shadeGrayColor * 0.2;
-      overlayImageData.data[i*4 + 3] = 255;
-    }
-  }
-
-  shadeContext.putImageData(shadeImageData, 0, 0);
-  overlayContext.putImageData(overlayImageData, 0, 0);
+  tf.dispose(overlayTensor);
 }
-
